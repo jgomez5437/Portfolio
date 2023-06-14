cb1c769382f93f11b271b4c05bae8133f4b3d679
Jesus Gomez Martinez <jesus.gomez5437@live.com>
2023-06-14 14:50:17 -0700

    Edited menu icon for mobile view

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const navbar = document.querySelector('.navbar');
 const nav_text = document.querySelector('.navLinks')
 const nav_text2 = document.querySelector('.navLinks2')
 const nav_text3 = document.querySelector('.navLinks3')
+const menu_icon = document.querySelector('#menuIcon')
+
 window.onscroll = () => {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 50) {
         navbar.classList.add('nav-active');
         nav_text.classList.add('nav_link');
         nav_text2.classList.add('nav_link');
         nav_text3.classList.add('nav_link');
+        menu_icon.src = "pictures/menu.png"
     } else {
         navbar.classList.remove('nav-active');
         nav_text.classList.remove('nav_link');
         nav_text2.classList.remove('nav_link');
         nav_text3.classList.remove('nav_link');
+        menu_icon.src = "pictures/menu-4-512.png"
 
     }
 };
